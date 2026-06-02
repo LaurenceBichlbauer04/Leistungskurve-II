c5e720b33f3a8a26b33800a1f71a2c0238e0c26c
laurencebichlbauer04 <lth.bichlbauer04@mci4me.at>
2026-06-02 10:28:08 +0200

    Push Powercurve etc

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from src import powercurve
+from src import load_data
 
 
 def main():
 
+    data = load_data.load_activity("data/activity.csv")
+    #print(data)
+    powercurve.curve_data(data)
 
 
 
 
-
-    return
 
 
 
--- src/powercurve.py
+++ src/powercurve.py
@@ -1,2 +1,29 @@
 import pandas as pd
 
+def curve_data(data):
+    
+    
+    time_list = []
+    max_avg_list = []
+    #print(time)
+    for i in range(len(data)+1):
+        max_avg = data["PowerOriginal"].rolling(i).mean().max()
+        if not pd.isna(max_avg):
+            time_list.append(i)
+            max_avg_list.append(max_avg)
+    df = pd.DataFrame({
+        "Time" : time_list,
+        "max_avg" : max_avg_list
+    })
+    #print(df)
+    return df
+        
+
+
+
+
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/activity.csv")
+    curve_data(df)
